eb73e8ff2942fb3858d71ce50687795adc9b9c52
PiaPetite <108560780+PiaPetite@users.noreply.github.com>
2021-03-04 16:58:05 +0000

    Minimal changes

--- datasets/lsun.py
+++ datasets/lsun.py
@@ -5,4 +5,15 @@ import os.path
 import io
 from collections.abc import Iterable
 import pickle
-from torchvision.datasets.utils import
+from torchvision.datasets.utils import verify_str_arg, iterable_to_str
+
+
+class LSUNClass(VisionDataset):
+    def __init__(self, root, transform=None, target_transform=None):
+        import lmdb
+
+        super(LSUNClass, self).__init__(
+            root, transform=transform, target_transform=target_transform
+        )
+
+        self.env = lmdb.o
