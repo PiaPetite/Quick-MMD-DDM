e9c14bb6696c87ccd7001dbdcb6d45053367fbd2
PiaPetite <108560780+PiaPetite@users.noreply.github.com>
2021-04-25 13:31:07 +0000

    stage-local

--- datasets/vision.py
+++ datasets/vision.py
@@ -55,4 +55,12 @@ class VisionDataset(data.Dataset):
         return ""
 
 
-class 
+class StandardTransform(object):
+    def __init__(self, transform=None, target_transform=None):
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __call__(self, input, target):
+        if self.transform is not None:
+            input = self.transform(input)
+        if self.target_transform is not No
