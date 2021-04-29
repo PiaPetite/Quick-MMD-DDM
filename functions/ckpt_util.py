0ad9ca87dd9bfc166b9ab8898630d8fb7847ef49
PiaPetite <108560780+PiaPetite@users.noreply.github.com>
2021-04-29 18:32:10 +0000

    remove wrong file

--- functions/ckpt_util.py
+++ functions/ckpt_util.py
@@ -7,4 +7,7 @@ URL_MAP = {
     "ema_cifar10": "https://heibox.uni-heidelberg.de/f/2e4f01e2d9ee49bab1d5/?dl=1",
     "lsun_bedroom": "https://heibox.uni-heidelberg.de/f/f179d4f21ebc4d43bbfe/?dl=1",
     "ema_lsun_bedroom": "https://heibox.uni-heidelberg.de/f/b95206528f384185889b/?dl=1",
-    "lsun_cat": "https://heibox.uni-heidelber
+    "lsun_cat": "https://heibox.uni-heidelberg.de/f/fac870bd988348eab88e/?dl=1",
+    "ema_lsun_cat": "https://heibox.uni-heidelberg.de/f/0701aac3aa69457bbe34/?dl=1",
+    "lsun_church": "https://heibox.uni-heidelberg.de/f/2711a6f712e34b06b9d8/?dl=1",
+    "ema_lsun_church": "https://heibox.uni-heidelberg.de/f/44ccb50e
