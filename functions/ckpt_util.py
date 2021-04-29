86a0c7858877090d8c4e362a92896c93b8b10785
PiaPetite <108560780+PiaPetite@users.noreply.github.com>
2021-04-29 12:14:36 +0000

    Readahead: Optimize divide/multiply by power of 2 using L/R shift

--- functions/ckpt_util.py
+++ functions/ckpt_util.py
@@ -4,4 +4,7 @@ from tqdm import tqdm
 
 URL_MAP = {
     "cifar10": "https://heibox.uni-heidelberg.de/f/869980b53bf5416c8a28/?dl=1",
-    "ema
+    "ema_cifar10": "https://heibox.uni-heidelberg.de/f/2e4f01e2d9ee49bab1d5/?dl=1",
+    "lsun_bedroom": "https://heibox.uni-heidelberg.de/f/f179d4f21ebc4d43bbfe/?dl=1",
+    "ema_lsun_bedroom": "https://heibox.uni-heidelberg.de/f/b95206528f384185889b/?dl=1",
+    "lsun_cat": "https://heibox.uni-heidelber
