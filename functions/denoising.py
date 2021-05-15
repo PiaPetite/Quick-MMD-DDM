8f3d7d469ceab553e097d3359d86fa10d0ab5299
PiaPetite <108560780+PiaPetite@users.noreply.github.com>
2021-05-15 07:34:23 +0000

    New translations ja.yml (English)

--- functions/denoising.py
+++ functions/denoising.py
@@ -5,4 +5,16 @@ import torch.utils.checkpoint as checkpoint
 def compute_alpha(beta, t):
 
     beta = torch.cat([torch.zeros(1).to(beta.device), beta], dim=0) #add 0 to the beginning of beta
-    a = (
+    a = (1 - beta).cumprod(dim=0).index_select(0, t + 1).view(-1, 1, 1, 1) #shape (n x 1 x 1 x 1)
+    return a
+
+def custom(module):
+    def custom_forward(*inputs):
+        out =  module(inputs[0], inputs[1])
+        return out
+    return custom_forward
+
+def generalized_steps_diff(x, seq, model, b, **kwargs):
+
+    """
+    Functi
