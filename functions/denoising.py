c0918439c88b7267d5c20500a4447de601114d3c
PiaPetite <108560780+PiaPetite@users.noreply.github.com>
2021-05-30 21:56:49 +0000

    Removed.

--- functions/denoising.py
+++ functions/denoising.py
@@ -92,3 +92,11 @@ def generalized_steps(x, seq, model, b, **kwargs):
             at = compute_alpha(b, t.long())
             at_next = compute_alpha(b, next_t.long())
             xt = xs[-1].to('cuda')
+            et = model(xt, t)
+            x0_t = (xt - et * (1 - at).sqrt()) / at.sqrt()
+            x0_preds.append(x0_t.to('cpu'))
+            c1 = (
+                kwargs.get("eta", 0) * ((1 - at / at_next) * (1 - at_next) / (1 - at)).sqrt()
+            )
+            c2 = ((1 - at_next) - c1 ** 2).sqrt()
+            x
