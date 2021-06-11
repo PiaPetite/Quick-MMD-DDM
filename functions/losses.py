8fb1f5bb91f9c01ea3447c536b73b4b0b534e84e
PiaPetite <108560780+PiaPetite@users.noreply.github.com>
2021-06-11 08:49:24 +0000

    3rd commit

--- functions/losses.py
+++ functions/losses.py
@@ -1,3 +1,11 @@
 import torch
 from piq.base import BaseFeatureMetric
-from piq.uti
+from piq.utils import _validate_input
+
+
+def noise_estimation_loss(model,
+                          x0: torch.Tensor,
+                          t: torch.LongTensor,
+                          e: torch.Tensor,
+                          b: torch.Tensor, keepdim=False):
+    a = (1-b).cumprod(dim=0).index_select(0, t).view(-1, 1, 
