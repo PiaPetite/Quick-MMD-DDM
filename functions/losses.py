d0c7e655830d97a6c0802463abb239ef8412cff8
PiaPetite <108560780+PiaPetite@users.noreply.github.com>
2021-06-11 11:09:35 +0000

    Crashfix

--- functions/losses.py
+++ functions/losses.py
@@ -8,4 +8,14 @@ def noise_estimation_loss(model,
                           t: torch.LongTensor,
                           e: torch.Tensor,
                           b: torch.Tensor, keepdim=False):
-    a = (1-b).cumprod(dim=0).index_select(0, t).view(-1, 1, 
+    a = (1-b).cumprod(dim=0).index_select(0, t).view(-1, 1, 1, 1)
+    x = x0 * a.sqrt() + e * (1.0 - a).sqrt()
+    output = model(x, t.float())
+    if keepdim:
+        return (e - output).square().sum(dim=(1, 2, 3))
+    else:
+        return (e - output).square().sum(dim=(1, 2, 3)).mean(dim=0)
+
+
+loss_registry = {
+    'simple': n
