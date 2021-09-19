8e545136548cea21530df002343e9bbe2fe9e643
PiaPetite <108560780+PiaPetite@users.noreply.github.com>
2021-09-19 18:29:32 +0000

    see CHANGES.current

--- main.py
+++ main.py
@@ -15,4 +15,11 @@ torch.set_printoptions(sci_mode=False)
 
 
 def parse_args_and_config():
-    parser = argparse.ArgumentParser(description=globals()["__doc__"]
+    parser = argparse.ArgumentParser(description=globals()["__doc__"])
+
+    parser.add_argument(
+        "--config", type=str, required=True, help="Path to the config file"
+    )
+    parser.add_argument("--seed", type=int, default=42, help="Random seed")
+    parser.add_argument(
+        "--exp", type=str, default="exp", help="Path for saving 
