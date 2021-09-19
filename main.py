9cfe6b51f812709931d372c572247d5d0edab298
PiaPetite <108560780+PiaPetite@users.noreply.github.com>
2021-09-19 16:10:07 +0000

    minor minor

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import argparse
 import traceback
 import shutil
+import logging
+import yaml
+import sys
+import os
+import torch
+import numpy as np
+import torch.utils.tensorboard as tb
+
+from runners.diffusion import Diffusion
+
+torch.set_printoptions(sci_mode=False)
+
+
+def parse_args_and_config():
+    parser = argparse.ArgumentParser(description=globals()["__doc__"]
