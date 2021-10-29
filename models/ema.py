3d8320cd6744dfc8f919ca182898cb66936ec194
PiaPetite <108560780+PiaPetite@users.noreply.github.com>
2021-10-29 20:24:29 +0000

    jdk

--- models/ema.py
+++ models/ema.py
@@ -2,4 +2,13 @@ import torch.nn as nn
 
 
 class EMAHelper(object):
-    def __
+    def __init__(self, mu=0.999):
+        self.mu = mu
+        self.shadow = {}
+
+    def register(self, module):
+        if isinstance(module, nn.DataParallel):
+            module = module.module
+        for name, param in module.named_parameters():
+            if param.requires_grad:
+                
