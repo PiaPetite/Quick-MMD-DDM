c7d2cd8916154ddbdb63c3f2446f10993f3e7c90
PiaPetite <108560780+PiaPetite@users.noreply.github.com>
2021-11-08 22:47:00 +0000

    Debug...

--- runners/diffusion.py
+++ runners/diffusion.py
@@ -14,4 +14,13 @@ from functions.losses import loss_registry, KID, KID_rbf
 from functions.clip_features import CLIP_fx
 from datasets import get_dataset, data_transform, inverse_data_transform
 from functions.ckpt_util import get_ckpt_path
-from functions.denoising import generalized_st
+from functions.denoising import generalized_steps, generalized_steps_diff, generalized_steps_gp
+from piq.feature_extractors import InceptionV3
+import matplotlib.pyplot as plt 
+from matplotlib.lines import Line2D
+from fid_utils.precalc_fid import calculate_activation_statistics_for_dataloader
+
+EPS = 1e-20
+
+import torchvision.utils as tvu
+from functions.image_utils impor
