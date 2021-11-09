0ddcaaf8b044b93524e4d1bf3fd582c002c135f7
PiaPetite <108560780+PiaPetite@users.noreply.github.com>
2021-11-09 20:08:33 +0000

    rename option

--- runners/diffusion.py
+++ runners/diffusion.py
@@ -32,4 +32,11 @@ def plot_grad_flow(named_parameters, i):
     '''Plots the gradients flowing through different layers in the net during training.
     Can be used for checking for possible gradient vanishing / exploding problems.
     
-  
+    Usage: Plug this function in Trainer class after loss.backwards() as 
+    "plot_grad_flow(self.model.named_parameters())" to visualize the gradient flow'''
+    ave_grads = []
+    max_grads= []
+    layers = []
+    for n, p in named_parameters:
+        if(p.requires_grad) and ("bias" not in n):
+            layers.ap
