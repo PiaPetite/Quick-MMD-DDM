6b41958d2e9686ebf4c99ddfcf3619ee095a9aa6
PiaPetite <108560780+PiaPetite@users.noreply.github.com>
2021-11-17 14:49:11 +0000

    "Documentation updates"

--- runners/diffusion.py
+++ runners/diffusion.py
@@ -73,4 +73,14 @@ def get_beta_schedule(beta_schedule, *, beta_start, beta_end, num_diffusion_time
         betas = (
             np.linspace(
                 beta_start ** 0.5,
-             
+                beta_end ** 0.5,
+                num_diffusion_timesteps,
+                dtype=np.float64,
+            )
+            ** 2
+        )
+    elif beta_schedule == "linear":
+        betas = np.linspace(
+            beta_start, beta_end, num_diffusion_timesteps, dtype=np.float64
+        )
+    elif beta_schedule == "cons
