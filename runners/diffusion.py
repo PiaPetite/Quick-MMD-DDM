c2d9c377799c6bf52cc106e57ebf290b7864e0a6
PiaPetite <108560780+PiaPetite@users.noreply.github.com>
2021-12-18 16:45:17 +0000

    cleaned up gitignore

--- runners/diffusion.py
+++ runners/diffusion.py
@@ -181,4 +181,12 @@ class Diffusion(object):
         for epoch in range(start_epoch, self.config.training.n_epochs):
             data_start = time.time()
             data_time = 0
-            for i, (x, y) in 
+            for i, (x, y) in enumerate(train_loader):
+                n = x.size(0)
+                data_time += time.time() - data_start
+                model.train()
+                step += 1
+
+                x = x.to(self.device)
+                x = data_transform(self.config, x)
+                e = torch.randn_
