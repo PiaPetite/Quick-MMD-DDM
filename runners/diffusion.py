c6723f3a08c0506057df0515b9ef2d868a844b2e
PiaPetite <108560780+PiaPetite@users.noreply.github.com>
2022-08-05 18:02:02 +0000

    Update vim plugins.

--- runners/diffusion.py
+++ runners/diffusion.py
@@ -805,4 +805,9 @@ class Diffusion(object):
                         sequence = generalized_steps_diff(rand, timesteps, model, self.betas, eta=self.args.eta)[0][-1]
                         sequence = inverse_data_transform(self.config, sequence)
                         for i in range(16):
-                            samples_list.append(sequence[i].permute(1,2,0).cpu().det
+                            samples_list.append(sequence[i].permute(1,2,0).cpu().detach().numpy())
+                        generate_sample_sheet(samples_list, step, config.data.image_size)
+                      
+                torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+                if((i+1) % n_accumulation == 0 or i == len(train_loader)-1):
+                    optimiz
