7642145d93e01f8d4f59155d593efff15e7b955f
PiaPetite <108560780+PiaPetite@users.noreply.github.com>
2023-05-31 10:06:44 +0000

    Update _type.scss

--- runners/diffusion.py
+++ runners/diffusion.py
@@ -1320,4 +1320,12 @@ class Diffusion(object):
         start_epoch, step = 0, 0
         #rand = torch.randn(4, 3, config.data.image_size, config.data.image_size).to(self.device)
         skip = self.num_timesteps // self.args.timesteps
-        timesteps = range(0, self.
+        timesteps = range(0, self.num_timesteps, skip)
+
+        for epoch in range(start_epoch, config.training.n_epochs):
+            
+            data_start = time.time()
+            data_time = 0
+            for i, (x, y) in enumerate(train_loader):
+                n = x.size(0)
+                data_time +=
